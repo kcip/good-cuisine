c546294f358e67b6c0f35872af28a5afde3b20e6
Scott <scottepick@gmail.com>
2020-06-20 16:31:54 -0400

    added components and styling for home component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
-const Search = () => {
+import './search.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+const Search = (props) => {
  return (
-  <div>
-   search
-  </div>
+  <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
+   <span className="fontAwesomeIcon search">
+    <FontAwesomeIcon icon={faSearch} color="#767676" />
+   </span>
+   <input
+    className="search-input"
+    value={props.value}
+    onChange={(e) => props.onChange(e)}
+    name="Search"
+    placeholder="Search..."
+    type="text"
+    autoFocus
+   />
+
+  </form>
  )
 }
 export default Search;
