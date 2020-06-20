5b226f20eafcb8e6bcae23e7024f1d2ff6bd17b1
杨晨珑 <yangchenlong@yangchenlongdeMacBook-Pro.local>
2020-06-20 16:50:20 -0400

    work in progress for category, subcategory, and cards

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import Home from './components/Home'
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
+import CategoryPage from './components/CategoryPage';
+import SubcategoryPage from './components/SubcategoryPage';
 
 function App() {
- return (
-  <div className="app">
+  return (
+    <div className="app">
 
-   <Switch >
-    <Route exact path='/' component={Home} />
-   </Switch>
-  </div>
- );
+      <Switch >
+        <Route exact path='/' component={Home} />
+        <Route exact path='/category/:category'>
+          <CategoryPage />
+        </Route>
+        <Route exact path="/category/:category/:subcategory">
+          <SubcategoryPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/RecipeCard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class RecipeCard extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          Recipe Cards for {this.props.subcategoryName}
+        </div>
+        <div>
+
+        </div>
+
+
+      </div>
+    )
+  }
+}
