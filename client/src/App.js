19cdc2efcfb08780b1488ab0b0e9093636dae4e0
Scott <scottepick@gmail.com>
2020-06-23 14:00:37 -0400

    added files

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,26 @@ import RecipeEdit from './components/RecipeEdit'
 import './App.css';
 import CategoryPage from './components/CategoryPage';
 import SubcategoryPage from './components/SubcategoryPage';
-
+import RecipeDetail from './components/RecipeDetail'
 function App() {
-  return (
-    <div className="app">
+ return (
+  <div className="app">
 
-      <Switch >
-        <Route exact path='/' component={Home} />
-        <Route exact path='/' component={Home} />
-        <Route exact path="/:id/edit" component={RecipeEdit} />
-        <Route exact path='/category/:category'>
-          <CategoryPage />
-        </Route>
-        <Route exact path="/category/:category/:subcategory">
-          <SubcategoryPage />
-        </Route>
-      </Switch>
-    </div>
+   <Switch >
+    <Route path='/' component={Home} />
+    <Route path='/:id' exact component={RecipeDetail} />
+    <Route exact path="/:id/edit" component={RecipeEdit} />
+    <Route exact path='/category/:category'>
+     <CategoryPage />
+    </Route>
+    <Route exact path="/category/:category/:subcategory">
+     <SubcategoryPage />
+    </Route>
+   </Switch>
+  </div>
 
 
-  );
+ );
 }
 
 export default App;
