0d9714115bd50f102c97ac73285022e167f1b50b
Christian Matos <chris@ChristisMacBook.fios-router.home>
2020-06-24 09:41:17 -0400

    woking

--- client/src/components/RecipeDetail.jsx
+++ client/src/components/RecipeDetail.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import Layout from './shared/Layout'
 import { getRecipe, deleteRecipe } from '../services/recipes'
+import Header from './shared/Header';
+import Footer from './shared/Footer'
+import Search from '../components/Search'
 import { Link } from 'react-router-dom'
 import './recipedetail.scss'
 
@@ -25,7 +28,9 @@ class RecipeDetail extends Component {
         keyword: '',
         _id: '',
         saved: null,
-      }
+
+      },
+      selector: "ingredients"
     }
   }
 
@@ -37,50 +42,74 @@ class RecipeDetail extends Component {
 
   }
 
+  toggles = (selection) => {
+    this.setState({
+      selector: selection
+    })
+    console.log(selection)
+  }
+
 
   render() {
     const { recipe } = this.state
 
     const steps = recipe.instructions && recipe.instructions
     const step = steps && steps.map((item, index) => {
-      return(
-      <div className="instruction">
-      <p>Step {index +1} {item}</p>
-    </div> 
-    )
+      return (
+        <div className="instruction">
+          <p>Step {index + 1} {item}</p>
+        </div>
+      )
     })
-    console.log(step)
+    const stuffs = recipe.ingredients && recipe.ingredients
+    const stuff = stuffs && stuffs.map((item) => {
+      return (
+        <div className="instruction">
+          <p> .  {item}</p>
+        </div>
+      )
+    })
+    const kitchens = recipe.equipment && recipe.equipment
+    const kitchen = kitchens && kitchens.map((item) => {
+      return (
+        <div className="kitchen">
+          <p> .  {item}</p>
+        </div>
+      )
+    })
     return (<>
+       <Header />
+     <Search />
       <div className="name">
-      <h2>{recipe.name}</h2>
+        <h2>{recipe.name}</h2>
       </div>
-      <div className="recipe-deatil">
-    
+      <div className="recipe-detail">
         <div className="vid">
           <iframe src={recipe.videoURL} width="1080px" height="607px"></iframe>
         </div>
-        <button class="tablinks" onclick={recipe.ingredients}>Ingredients</button>
-  <button class="tablinks" onclick="openCity(event, 'Equipment')">Equipment</button>
-        <div className="container">
-        <div id={recipe.ingredients} class="tabcontent">
-        
-        </div>
-        <div id="Equipment" class="tabcontent">
-          <p>equipment:{recipe.equipment}</p>
-          </div>
-          </div>
-        
-        <div className="instructions">
-          {step}
 
-        </div>
+        <button class="tablinks" onClick={() => { this.toggles("ingredients") }}>Ingredients</button>
+        <button class="tablinks" onClick={() => { this.toggles("equipment") }}>Equipment</button>
+        {this.state.selector == "ingredients" ?
+          <div className="in">
+            {stuff}
+          </div> :
+          <div className="equp">
+            {kitchen}
+          </div>
+        }
+      </div>
+      <div className="instructions">
+        {step}
       </div>
       <div className="button-container">
         <button className="edit-button">
           <Link className="edit-link" to={`${this.state.recipe._id}/edit`}>Edit</Link>
         </button>
         <button className="delete-button" onClick={() => deleteRecipe(this.state.recipe._id)}>Delete</button>
+
       </div>
+      <Footer />
     </>
     )
   }
